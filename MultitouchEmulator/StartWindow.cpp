917534d7439e24a134dc9bcbdb4ab7b260932ee8
kn65op <kn65op@gmail.com>
2012-07-05 12:16:56 +0200

    Added class sartWindow

---
+++ MultitouchEmulator/StartWindow.cpp
@@ -0,0 +1,12 @@
+#include "StdAfx.h"
+#include "StartWindow.h"
+
+
+StartWindow::StartWindow(void)
+{
+}
+
+
+StartWindow::~StartWindow(void)
+{
+}
---
+++ MultitouchEmulator/StartWindow.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+
+class StartWindow : public Gtk::Dialog
+{
+public:
+  StartWindow(void);
+  ~StartWindow(void);
+private:
+  //choose field
+  Gtk::
+};
+
